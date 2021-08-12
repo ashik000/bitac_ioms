f1c7a260508cf4919d22c2e32f790bb23848f2c2
shuvo <shuvo.inovace@inovace.com>
2021-08-12 14:49:29 +0600

    stations->products tab style and functionality

--- resources/js/components/station/StationProduct.vue
+++ resources/js/components/station/StationProduct.vue
@@ -1,15 +1,15 @@
 <template>
     <div>
         <header class="section-header">
-            <span style="color: #ffffff">
-                Products
-            </span>
-            <button class="btn" style="color: #ffffff" @click="openStationProductCreateModal()">
-                <i class="icon material-icons" style="font-size:24px; color:white;">add_circle_outline</i>
+            <button class="btn btn-primary btn-sm btn-assign-product" @click="openStationProductCreateModal()">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+                <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z"></path>
+              </svg>
+              Assign Product
             </button>
         </header>
         <div>
-            <table class="settings-table table small" v-if="showStationProductsTable">
+            <table class="table table-bordered station-product-table" v-if="showStationProductsTable">
                 <thead>
                     <tr>
                         <th>Name</th>
@@ -18,28 +18,32 @@
                         <th>Timeout</th>
                         <th>Units/Signal</th>
                         <th>Threshold</th>
-                        <th class="text-center">Actions</th>
+                        <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                 <tr v-for="stationProduct in stationProducts" :key="stationProduct.id">
                     <td>{{stationProduct.product.name}}</td>
                     <td>{{stationProduct.cycle_time}}</td>
-                    <td style="text-transform: lowercase">{{stationProduct.cycle_unit}}</td>
+                    <td>{{stationProduct.cycle_unit}}</td>
                     <td>{{stationProduct.cycle_timeout}}</td>
                     <td>{{stationProduct.units_per_signal}}</td>
                     <td>{{stationProduct.performance_threshold}}</td>
                     <td>
-                        <div class="d-inline-flex">
-                            <a class="btn btn-link">
-                                <i class="material-icons" @click.prevent="openStationProductEditForm(stationProduct)">
-                                    edit
-                                </i>
+                        <div class="d-flex station-product-actions-btn-group">
+                            <a class="btn btn-primary btn-sm" @click.prevent="openStationProductEditForm(stationProduct)">
+                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"></path>
+                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"></path>
+                              </svg>
+                                    EDIT
                             </a>
-                            <a class="btn btn-link">
-                                <i class="material-icons" @click.prevent="openStationProductDeleteForm(stationProduct)">
-                                    delete
-                                </i>
+                            <a class="btn btn-danger btn-sm" @click.prevent="openStationProductDeleteForm(stationProduct)">
+                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
+                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
+                              </svg>
+                                    REMOVE
                             </a>
                         </div>
                     </td>
@@ -50,7 +54,7 @@
         <Modal v-if="showStationProductModal == true" @close="closeStationProductModal()">
             <template v-slot:header>
                 <div class="text-center">
-                    <h5>{{stationProduct.id == null ? 'Add Station Product' : 'Edit Station Product'}}</h5>
+                    <h5>{{stationProduct.id == null ? 'Assign Station Product' : 'Edit Station Product'}}</h5>
                 </div>
             </template>
             <template v-slot:content>
@@ -215,56 +219,3 @@
         }
     }
 </script>
-
-<style scoped lang="scss">
-    .section-header {
-        padding: 0.5rem 1rem;
-        background: #232156;
-
-        display: flex;
-        flex-direction: row;
-        justify-content: flex-start;
-        align-items: center;
-
-        > span {
-            flex-grow: 1;
-        }
-
-        .action {
-            background: #0c4da1;
-            border: None;
-            border-radius: 50%;
-
-            padding: 0;
-            width: 1.5rem;
-            height: 1.5rem;
-
-            font-size: 1.25rem;
-            line-height: 1.5rem;
-
-            box-shadow: 1px 1px 1px 1px #0c4da1;
-        }
-    }
-
-    .settings-table {
-        thead {
-            background: #4e4d78;
-            color: black;
-            opacity: .7;
-            th {
-                padding: 0.25rem;
-                font-weight: bold;
-                border: 1px solid #0B312A;
-            }
-        }
-
-        tbody {
-            td {
-                background: transparent;
-                border: 1px solid #0B312A;
-                color: black;
-            }
-        }
-    }
-</style>
-
