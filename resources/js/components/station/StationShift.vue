c1d251773fee233bdb31aa561f91ef1ebb01e9c2
Luniam <mainul.i.mahi@gmail.com>
2021-08-17 17:02:56 +0600

    Station shift create modal bug fixed

--- resources/js/components/station/StationShift.vue
+++ resources/js/components/station/StationShift.vue
@@ -2,7 +2,7 @@
     <div>
         <header class="section-header">
 
-            <button class="btn btn btn-primary btn-sm btn-assign-item" @click="showStationShiftForm = true">
+            <button class="btn btn btn-primary btn-sm btn-assign-item" @click="openCreateStationShiftModal()">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
                     <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z"></path>
                 </svg>
@@ -137,6 +137,12 @@
             toggleSelectedShiftDays(dayIndex) {
                 let val = this.selectedShiftDays[dayIndex] === '1' ? '0' : '1';
                 Vue.set(this.selectedShiftDays, dayIndex, val);
+            },
+
+            openCreateStationShiftModal() {
+                this.stationShiftEditMode = false;
+                for(var i = 0; i < 7; i++) Vue.set(this.selectedShiftDays, i, '0');
+                this.showStationShiftForm = true;
             },
 
             openEditStationShiftModal(stationShift){
