3c0f9e41731b8bf2ddbc2db693674b3c4971e96c
Risalat Zaman <risalat.inovace@gmail.com>
2021-12-19 17:37:35 +0600

    Added team list api

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Data\Models\Operator;
 use App\Data\Models\Team;
+use DB;
 use Illuminate\Http\Request;
 
 class TeamController extends Controller
 {
     public function index(Request $request){
         return Team::all();
+    }
+
+    public function getOperatorsOfTeam(Request $request){
+        $team_id = $request['team_id'];
+        return DB::table('team_operator')
+            ->join('operators', 'operators.id', '=', 'team_operator.operator_id')
+            ->where('team_operator.team_id', '=', $team_id)
+            ->get();
+    }
+
+    public function store(Request $request){
+        return Team::create([
+            'name' => $request['name']
+        ]);
     }
 }
