1f6b1267f4fab98ee45e4ca7787e5e82a8390392
Ashik <ashik000@yahoo.com>
2022-01-18 11:43:57 +0600

    dashboard operator name fix

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -329,20 +329,16 @@ export default {
         },
 
         getOperator() {
-            var currentDate = new Date();
-            console.log(currentDate);
-            var formatted_date = new Date().toJSON();
-            console.log(formatted_date);
+            let currentDate = new Date().toJSON().slice(0,10).replace(/-/g,'/');
             axios.get('getOperatorName', {
                 params: {
                     stationId: 7,
-                    date: formatted_date
+                    date: currentDate
                 }
             })
 
                 .then((response) => {
                     this.operatorName = response.data.operatorName
-                    console.log(response.data)
                 })
         }
     },
